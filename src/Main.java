511ae89358207bce23a75941714c6683a8e4e710
Liudmila <milalindley@gmail.com>
2025-12-17 20:03:18 +0100

    fix: remove unused import

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
