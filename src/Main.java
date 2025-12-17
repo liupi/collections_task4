68a72ea6d8c64dc30f39a024d92ca562813f9f80
Liudmila <milalindley@gmail.com>
2025-12-17 19:50:25 +0100

    feat: print data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Employee> employees = readFile().stream()
+                .filter(e -> !e.isBlank())
+                .map(str -> {
+                    String[] arr = str.trim().split(" ");
+                    return new Employee(arr[0], arr[1]);
+                }).toList();
 
+        System.out.println("Size of collection: " + employees.size());
+
+        System.out.println("\nPrint employees in reversed order:");
+        employees.forEach(System.out::println);
+    }
+
+    private static List<String> readFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("resources/employees.txt"))) {
+            List<String> list = new ArrayList<>(br.lines().toList());
+            list.sort(Comparator.reverseOrder());
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file.");
+        }
     }
 }
